dcd93feb316b40fdcf59f3abe79a1ba50d98a2f2
Felix-Busse <f.busse@posteo.de>
2024-02-26 20:57:55 +0100

    Work on data_collect_app containerization

--- data_collect_app/main.py
+++ data_collect_app/main.py
@@ -1,7 +1,14 @@
-from crontab import CronTab
+import finrail_db
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
 
-cron = CronTab(user=True)
-job = cron.new('touch test.py')
-job.minute.every(1)
-cron.write()
-job.run()
+# Add path of subdirectory containing own modules
+#modules_path = os.path.join(os.getcwd(), 'app', 'cron')
+#if modules_path not in sys.path:
+#    sys.path.append(modules_path)
+engine = finrail_db.create_tables()
+ession = sessionmaker(bind=engine)
+session = Session()
+
