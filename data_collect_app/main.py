f13724c7ccae4afdce8fbe1a8737434384b975e3
Felix-Busse <f.busse@posteo.de>
2024-02-28 14:24:46 +0100

    Finalized Dockerfile and docker-compose.yaml

--- data_collect_app/main.py
+++ data_collect_app/main.py
@@ -3,14 +3,20 @@ import finrail_db
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
 
-# Add path of subdirectory containing own modules
-#modules_path = os.path.join(os.getcwd(), 'app', 'cron')
-#if modules_path not in sys.path:
-#    sys.path.append(modules_path)
-engine = finrail_db.create_tables()
+# Read environment variables needed to connect to database
+db_psw = os.environ['DB_PSW']
+db_user = os.environ['DB_USER']
+db_name = os.environ['DB_NAME']
+db_server = os.environ['DB_SERVER']
+
+# Create database string from content of variables
+db_str = f'mysql+mysqlconnector://{db_user}:{db_psw}@{db_server}/{db_name}'
+
+# Create database engine and open session
+engine = finrail_db.create_tables(db_str)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# Pass session to update data base with missing data, use today as end date
 finrail_db.add_compositions(s=session, date_end=dt.date.today())
